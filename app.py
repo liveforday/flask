73fbaebd2795241802df22ec4ad0c6b2bdc6d7b3
liveforday <34528658+liveforday@users.noreply.github.com>
2020-03-29 13:23:44 +0800

    user authentication with flask-login

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ import sys
 import click
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import (
+    current_user,
+    login_required,
+    LoginManager,
+    UserMixin,
+    login_user,
+    logout_user,
+)
 
 # SQLite URI compatible
 WIN = sys.platform.startswith("win")
@@ -19,6 +28,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(app.root_path, "da
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
 
 
 @app.cli.command()
@@ -60,9 +77,43 @@ def forge():
     click.echo("Done.")
 
 
-class User(db.Model):
+@app.cli.command()
+@click.option("--username", prompt=True, help="The username used to login.")
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    confirmation_prompt=True,
+    help="The password used to login",
+)
+def admin(username, password):
+    db.create_all
+    user = User.query.first()
+    if user is not None:
+        click.echo("Updating user ...")
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo("Creating user ...")
+        user = User(username=username, name="Admin")
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo("Done.")
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):
@@ -85,6 +136,9 @@ def page_not_found(e):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        if not current_user.is_authenticated:
+            return redirect(url_for("index"))
+
         title = request.form["title"]
         year = request.form["year"]
 
@@ -103,6 +157,7 @@ def index():
 
 
 @app.route("/movie/edit/<int:movie_id>", methods=["GET", "POST"])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -124,9 +179,39 @@ def edit(movie_id):
 
 
 @app.route("/movie/delete/<int:movie_id>", methods=["POST"])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
     flash("Item deleted.")
+    return redirect(url_for("index"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if not username or not password:
+            flash("Invalid input")
+            return redirect(url_for("login"))
+
+        user = User.query.first()
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash("Login success")
+            return redirect(url_for("index"))
+        flash("Invalid username or password")
+        return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Goodbye")
     return redirect(url_for("index"))
